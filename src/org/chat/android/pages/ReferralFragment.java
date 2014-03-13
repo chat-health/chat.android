2f22f4973395a1499b05b03bef90d9e659048bb5
colinmccann <colindmccann@gmail.com>
2014-03-13 10:50:40 -0400

    completed referrals page - tentative. Waiting on Lisa

--- src/org/chat/android/pages/ReferralFragment.java
+++ src/org/chat/android/pages/ReferralFragment.java
@@ -18,18 +18,15 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class ReferralFragment extends Fragment {
-	Context context;
-    TextView content1 = null;
-    TextView content2 = null;
-    TextView content3 = null;
-	
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     	View view = inflater.inflate(R.layout.fragment_referral, container, false);
-    	context = getActivity();
+    	Context context = getActivity();
     	
-    	content1 = (TextView) view.findViewById(R.id.r1tv1);
-		content2 = (TextView) view.findViewById(R.id.r1tv2);
-		content3 = (TextView) view.findViewById(R.id.r1tv3);
+    	TextView content1 = (TextView) view.findViewById(R.id.r1tv1);
+    	TextView content2 = (TextView) view.findViewById(R.id.r1tv2);
+    	TextView content3 = (TextView) view.findViewById(R.id.r1tv3);
+    	TextView content4 = (TextView) view.findViewById(R.id.r1tv4);
     	
     	// determine language from current tablet settings
     	String lang = Locale.getDefault().getLanguage();
@@ -38,18 +35,32 @@ public class ReferralFragment extends Fragment {
     	int clientId = getArguments().getInt("clientId");
     	List<HealthSelectRecorded> selects = ModelHelper.getHealthSelectRecordedsForVisitIdAndTopicNameAndClientId(context, visitId, "assessment", clientId);
     	
+    	// if this gets any more complicated (waiting on Lisa for design), create class/model for this - see below
     	for (HealthSelectRecorded hsr : selects) {
-    		if (hsr.getSelectId() == 10005) {
+    		int id = hsr.getSelectId();
+    		if (id == 10003 || id == 10007 || id == 10009 || id == 10011 || id == 10016 || id == 10019 || id == 10024 || id == 10025 || id == 10027 || id == 10029 || id == 10031 || id == 10033) {
     			content1.setVisibility(View.VISIBLE);
     		}
-    		if (hsr.getSelectId() == 10008) {
+    		if (id == 10005) {
     			content2.setVisibility(View.VISIBLE);
     		}
-    		if (hsr.getSelectId() == 10025 || hsr.getSelectId() == 10027) {
+    		if (id == 10008) {
     			content3.setVisibility(View.VISIBLE);
+    		}
+    		if (id == 10025 || id == 10027) {
+    			content4.setVisibility(View.VISIBLE);
     		}
     	}
     	
     	return view;
     }
 }
+
+
+/*
+Class ReferralTrigger
+int id: 1
+int triggerId: 10005
+String dangerSign: cough
+String enContent: some stuff about ors
+*/
