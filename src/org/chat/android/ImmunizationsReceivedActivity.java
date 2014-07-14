dd6d6ea1ec39d9820992569ef5c5ba5db049810d
colinmccann <colindmccann@gmail.com>
2014-07-14 18:23:40 -0400

    immunization update - ugly as hell for now, waiting on redesign input before finalizing

--- src/org/chat/android/ImmunizationsReceivedActivity.java
+++ src/org/chat/android/ImmunizationsReceivedActivity.java
@@ -102,11 +102,11 @@ public class ImmunizationsReceivedActivity extends BaseActivity {
 				row.setVisibility(View.VISIBLE);
 				ImageView flag = (ImageView)row.getChildAt(0);
 				TextView shortName = (TextView)row.getChildAt(1);
-				TextView longName = (TextView)row.getChildAt(2);
-				Button dateBtn = (Button)row.getChildAt(3);
+				//TextView longName = (TextView)row.getChildAt(2);
+				Button dateBtn = (Button)row.getChildAt(2);
 				
 				shortName.setText(v.getShortName());
-				longName.setText(v.getLongName());
+				//longName.setText(v.getLongName());
 				dateBtn.setTag(v.getId());
 				
 				VaccineRecorded vr = ModelHelper.getVaccineRecordedForClientIdAndVaccineId(context, client.getId(), v.getId());
@@ -118,7 +118,7 @@ public class ImmunizationsReceivedActivity extends BaseActivity {
 				} else {
 					flag.setVisibility(View.VISIBLE);
 					shortName.setTextColor(Color.parseColor("#d4145a"));
-					longName.setTextColor(Color.parseColor("#d4145a"));
+					//longName.setTextColor(Color.parseColor("#d4145a"));
 				}
 	    	}
     	} else {
